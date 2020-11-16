a3a4717b79052c56f2fd9a5f38c3219b7a081240
Denis Saenko <dsaenko@yandex.ru>
2020-11-16 11:49:24 +0100

    first commit

---
+++ src/main/java/com/example/submodules_training/SubmodulesTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.submodules_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmodulesTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmodulesTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/submodules_training/SubmodulesTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submodules_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmodulesTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
